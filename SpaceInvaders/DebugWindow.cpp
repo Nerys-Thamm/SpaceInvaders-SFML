d2db6824396e8da76a2c94bed588329f49e43cc3
Nerys <NerysThamm@gmail.com>
2020-10-01 11:52:51 +1300

    DONE

--- SpaceInvaders/DebugWindow.cpp
+++ SpaceInvaders/DebugWindow.cpp
@@ -7,8 +7,9 @@
 
 DebugWindow::DebugWindow()
 {
-	window = new sf::RenderWindow(sf::VideoMode(400, 400), "Space Invaders - Debug");
+	window = new sf::RenderWindow(sf::VideoMode(400, 400), "Space Invaders - Debug"); //Create the Debug Window
 	
+	//Config the Text
 	font.loadFromFile("Resources/sansation.ttf");
 	instructions.setFillColor(sf::Color::White);
 	instructions.setPosition(5, 0);
@@ -40,27 +41,28 @@ DebugWindow::DebugWindow()
 
 DebugWindow::~DebugWindow()
 {
-	window->close();
+	window->close();//Close and delete the window to free up the memory
 	delete window;
 }
 
 void DebugWindow::Update(float _fDeltaTime)
 {
-	while (window->pollEvent(wEvent))
+	while (window->pollEvent(wEvent))//for each event
 	{
-		switch (wEvent.type)
+		switch (wEvent.type) //If the event is...
 		{
-		case sf::Event::Closed:
+		case sf::Event::Closed://The window being closed, then close the window
 			window->close();
 			
 			break;
-		case sf::Event::KeyPressed:
+		case sf::Event::KeyPressed://A key being pressed, then...
 			switch (wEvent.key.code)
 			{
-			case sf::Keyboard::Escape:
-				CGame::bDebugMode = false;
+			case sf::Keyboard::Escape: //If the key is escape..
+				CGame::bDebugMode = false; //turn debug mode off
 				break;
-			case sf::Keyboard::Up:
+			case sf::Keyboard::Up: //If the key is Up..
+				//Cycle upwards through the options
 				if (iCurrentIndex > 0)
 				{
 					iCurrentIndex--;
@@ -70,7 +72,8 @@ void DebugWindow::Update(float _fDeltaTime)
 					iCurrentIndex = 3;
 				}
 				break;
-			case sf::Keyboard::Down:
+			case sf::Keyboard::Down: //If the key is Down
+				//Cycle downwards through the options
 				if (iCurrentIndex < 3)
 				{
 					iCurrentIndex++;
@@ -81,11 +84,11 @@ void DebugWindow::Update(float _fDeltaTime)
 				}
 
 				break;
-			case sf::Keyboard::Left:
+			case sf::Keyboard::Left: //If the key is Left...
 				switch (iCurrentIndex)
 				{
-				case 0:
-					if (CGame::score >= 10)
+				case 0: //and Score is selected..
+					if (CGame::score >= 10) //Decrease score by 10 to a minimum of 0
 					{
 						CGame::score -= 10;
 					}
@@ -94,20 +97,20 @@ void DebugWindow::Update(float _fDeltaTime)
 						CGame::score = 0;
 					}
 					break;
-				case 1:
-					if (CGame::iPlayerLives > 0)
+				case 1: //and Lives are selected..
+					if (CGame::iPlayerLives > 0) //Decrease lives by 1 to a minimum of 0
 					{
 						CGame::iPlayerLives--;
 					}
 					break;
-				case 2:
-					if (CGame::iAlienSpeedMult > -2)
+				case 2: //and Alien Speed Multiplier is selected..
+					if (CGame::iAlienSpeedMult > -2) //Decrease Alien speed multiplier by 1 to a minimum of -2
 					{
 						CGame::iAlienSpeedMult--;
 					}
 					break;
-				case 3:
-					if (CGame::iBulletSpeedMult > -2)
+				case 3: //and Bullet speed multiplier is selected..
+					if (CGame::iBulletSpeedMult > -2) //Decrease bullet speed multiplier by 1 to a minimum of -2
 					{
 						CGame::iBulletSpeedMult--;
 					}
@@ -117,23 +120,23 @@ void DebugWindow::Update(float _fDeltaTime)
 				}
 
 				break;
-			case sf::Keyboard::Right:
+			case sf::Keyboard::Right: //If the key is Right...
 				switch (iCurrentIndex)
 				{
-					case 0:
-						CGame::score += 10;
+					case 0: //And score is selected..
+						CGame::score += 10; //Increase score by 10
 						break;
-					case 1:
-						CGame::iPlayerLives++;
+					case 1: //And Lives are selected..
+						CGame::iPlayerLives++; //Increase lives by 1
 						break;
-					case 2:
-						if (CGame::iAlienSpeedMult < 5)
+					case 2: //And Alien speed multiplier is selected..
+						if (CGame::iAlienSpeedMult < 5) //Increase alien speed multiplier by 1 to a maximum of 5
 						{
 							CGame::iAlienSpeedMult++;
 						}
 						break;
-					case 3:
-						if (CGame::iBulletSpeedMult < 5)
+					case 3: //And Bullet speed multiplier is selected..
+						if (CGame::iBulletSpeedMult < 5) //Increaase bullet speed multiplier by 1 to a maximum of 5
 						{
 							CGame::iBulletSpeedMult++;
 						}
@@ -151,8 +154,9 @@ void DebugWindow::Update(float _fDeltaTime)
 		}
 
 	}
-	window->clear();
+	window->clear(); //Clear the debug window
 	
+	//Update the Text 
 	Options[0].setString("> Score: " + std::to_string(CGame::score));
 	Options[1].setString("> Lives: " + std::to_string(CGame::iPlayerLives));
 	Options[2].setString("> Alien Speed: " + std::to_string(CGame::iAlienSpeedMult));
@@ -162,15 +166,16 @@ void DebugWindow::Update(float _fDeltaTime)
 	Options[2].setFillColor(sf::Color::White);
 	Options[3].setFillColor(sf::Color::White);
 
+	//Give the selected option a green colouring and selection marker
 	Options[iCurrentIndex].setString("$/:" + Options[iCurrentIndex].getString() + "  -/+");
 	Options[iCurrentIndex].setFillColor(sf::Color::Green);
 
-	for each (sf::Text text in Options)
+	for each (sf::Text text in Options) //Draw all the text
 	{
 		window->draw(text);
 	}
 	window->draw(instructions);
-	window->display();
+	window->display(); //Display the frame
 
 }
 
